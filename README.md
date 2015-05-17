e79a8aceef4ac69b47bc25e9d6b98d2388c84aa5
shimataro <shimataro@zelkova.cc>
2015-05-17 09:08:27 +0900

    * add jspp.py

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# jspp
-C-like JavaScript Preprocessor Parser
+jspp
+======
+
+C-like JavaScript Preprocessor Parser.
+It enables to include other file into JavaScript file, like C preprocessor `#include`.
+And can combine multiple JavaScript files into one easily.
+
+## Requires
+
+* [Python](https://www.python.org/) (2.7 or 3.2 or 3.3 or 3.4)
+
+## How to use
+
+`//#include "filename"` will be replaced with the target filename.
+
+base.js
+```js
+a = 1;
+//#include "other.js"
+b = 2;
+```
+
+other.js
+```js
+x = 1;
+y = 2;
+```
+
+python jspp.py \<base.js
+```js
+a = 1;
+x = 1;
+y = 2;
+b = 2;
+```
+
+## Command line options
+
+* `-i <input-filename>`, `--input <input-filename>`
+
+Specify input filename.
+If omitted, stdin will be used.
+
+* `-o <output-filename>`, `--output <output-filename>`
+
+Specify output filename.
+If omitted, stdout will be used.
---
+++ jspp.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python2.7
+# coding: utf-8
+""" JavaScript Preprocessor Parser
+
+format:
+$ jspp [options]
+
+options:
+    -i|--input <input-filename>
+    -o|--output <output-filename>
+"""
+import os, sys, re, getopt
+re_include = re.compile(r"^\s*//\s*#include\s+\"(.+)\"\s*$")
+
+# status code
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
+# http://www.freebsd.org/cgi/man.cgi?query=sysexits
+EX_USAGE = 64
+EX_IOERR = 74
+
+def main():
+    """ main function """
+    try:
+        # parse options
+        options = parse_args()
+
+        file_in, file_out = get_inout(options)
+        with file_in, file_out:
+            parse_file(file_in, file_out)
+
+        return EXIT_SUCCESS
+
+    except getopt.GetoptError as err:
+        # option error
+        print_err(err.msg)
+        return EX_USAGE
+
+    except IOError as err:
+        # file error
+        print_err("{message}: {filename}".format(message = err.strerror, filename = err.filename))
+        return EX_IOERR
+
+
+def parse_args():
+    """ parse arguments
+
+    @return: parse result
+    @raise: getopt.GetoptError
+    """
+    opts, args = getopt.getopt(sys.argv[1:], "i:o:", ["input=", "output="])
+    options = {}
+    for o, a in opts:
+        if o in ("-i", "--input"):
+            # input file
+            options["input" ] = a
+            continue
+
+        if o in ("-o", "--output"):
+            # output file
+            options["output"] = a
+            continue
+
+    return options
+
+
+def get_inout(options):
+    """ get input/output objects
+
+    @param options: parse result
+    @return: input, output (specified files or stdin/stdout)
+    @raise: IOError
+    """
+    # open files
+    file_in  = sys.stdin
+    file_out = sys.stdout
+    try:
+        if "input"  in options:
+            file_in  = open(options["input"] , "r")
+            dirname = os.path.dirname(options["input"])
+            if dirname != "":
+                os.chdir(dirname)
+
+        if "output" in options:
+            file_out = open(options["output"], "w")
+
+        return file_in, file_out
+
+    except:
+        file_in .close()
+        file_out.close()
+        raise
+
+
+def parse_file(file_in, file_out):
+    """ parse file and output result
+
+    @param file_in: input file object
+    @param file_out: output file object
+    @raise: IOError
+    """
+    for line in file_in:
+        # check preprocessor
+        m = re_include.match(line)
+        if not m:
+            # no match
+            file_out.write(line)
+            continue;
+
+        # save current directory
+        curdir = os.getcwd()
+
+        # recursive call!
+        filename = m.group(1)
+        with open(filename, "r") as file_in_new:
+            dirname = os.path.dirname(filename)
+            if dirname != "":
+                os.chdir(dirname)
+
+            parse_file(file_in_new, file_out)
+
+        # restore current directory
+        os.chdir(curdir)
+
+
+def print_err(message):
+    """ output message (and linefeed) to stdout
+
+    @param message: message to be output
+    """
+    sys.stderr.write(message)
+    sys.stderr.write("\n")
+
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
